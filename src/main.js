7a139701c60d7d2652fdc823b2c931cdb37ffc72
jianming.zhou <zjm90s@163.com>
2024-07-01 23:59:29 +0800

    rm unocss

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import "./styles/index.scss"
-// import "uno.css"
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -4,38 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-import Unocss from 'unocss/vite'
-import {
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [
     vue(),
-    VueDevTools(),
-
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    // Unocss({
-    //   presets: [
-    //     presetUno(),
-    //     presetAttributify(),
-    //     presetIcons({
-    //       scale: 1.2,
-    //       warn: true,
-    //     }),
-    //   ],
-    //   transformers: [
-    //     transformerDirectives(),
-    //     transformerVariantGroup(),
-    //   ]
-    // }),
+    VueDevTools()
   ],
   resolve: {
     alias: {
